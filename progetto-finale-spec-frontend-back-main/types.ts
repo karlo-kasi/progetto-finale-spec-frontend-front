bc488259c95bde4c7d89d24d11391c70a1f1d501
karlo-kasi <karlokasi1995@gmail.com>
2025-06-26 12:50:08 +0200

    add types.ts for define the type electricCars and update .gitignore fro remove types.ts

---
+++ progetto-finale-spec-frontend-back-main/types.ts
@@ -0,0 +1,17 @@
+export type electricCars = {
+    title: string,
+    category: string,
+    anno: number,
+    prezzo: number,
+    autonomiaKm: number,
+    batteriaKWh: number,
+    ricaricaRapidaKw?: number,
+    accelerazioneSec: number,
+    velocit\u00e0MassimaKmH: number,
+    tipoMotore: string,
+    consumoKWh100Km: number,
+    bagagliaioLitri: number,
+    posti: number,
+    pesoKg: number,
+    immagini: string[]
+}
